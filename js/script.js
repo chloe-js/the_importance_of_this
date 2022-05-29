c6f1f9a2348d7475136ce411cf626477e7c78284
chloë js <chloejs.horta@gmail.com>
2022-05-29 09:34:39 +0000

    button hover in and out mouse

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
-$(document).ready(function() {
-    $(".box").on("click", function() {
-    	/**
-    	 * When we click on an element that has
-    	 * a 'box' class, this event will be fired.
-    	 */
-    	var classNames = $(this).attr("class").split(" ");
-        $("." + classNames[1]).css("background-color", "red");
-    });
+// $(document).ready(function() {
+//     $(".box").on("click", function() {
+//     	/**
+//     	 * When we click on an element that has
+//     	 * a 'box' class, this event will be fired.
+//     	 */
+//     	var classNames = $(this).attr("class").split(" ");
+//         $("." + classNames[1]).css("background-color", "red");
+//     });
+// });
+
+$('button').mouseenter(function() {
+	$(this).removeClass('makeRed').addClass('makeBlue');
+});
+
+$('button').mouseleave(function() {
+	$(this).removeClass('makeBlue').addClass('makeRed');
 });
