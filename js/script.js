113ca95ec2a88406f223f35935ed53c99cdf87ac
chloë js <chloejs.horta@gmail.com>
2022-05-29 09:41:30 +0000

    revert back to div change to red on click

--- js/script.js
+++ js/script.js
@@ -1,18 +1,11 @@
-// $(document).ready(function() {
-//     $(".box").on("click", function() {
-//     	/**
-//     	 * When we click on an element that has
-//     	 * a 'box' class, this event will be fired.
-//     	 */
-//     	var classNames = $(this).attr("class").split(" ");
-//         $("." + classNames[1]).css("background-color", "red");
-//     });
-// });
-
-$('button').mouseenter(function() {
-	$(this).removeClass('makeRed').addClass('makeBlue');
+$(document).ready(function() {
+    $(".box").on("click", function() {
+    	/**
+    	 * When we click on an element that has
+    	 * a 'box' class, this event will be fired.
+    	 */
+    	var classNames = $(this).attr("class").split(" ");
+        $("." + classNames[1]).css("background-color", "red");
+    });
 });
 
-$('button').mouseleave(function() {
-	$(this).removeClass('makeBlue').addClass('makeRed');
-});
